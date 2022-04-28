6aab2a7ba86d07e88f6633dca2d12ecd7ba799fd
Scott Goodrow <sgoodrow@gmail.com>
2022-04-27 21:45:17 -0700

    refactor: DRY up the invite commands

--- src/features/invite-list/friend-config-button.ts
+++ src/features/invite-list/friend-config-button.ts
@@ -1,40 +1,15 @@
-import { ButtonInteraction, CacheType } from "discord.js";
-import { dataSource } from "../../db/data-source";
 import { Invite } from "../../db/invite";
-import { ButtonCommand } from "../../shared/command/button-command";
-import { sortInvites } from "./update-action";
+import { PendingButtonCommand } from "./who-pending-button-command";
 
-class FriendConfigButtonCommand extends ButtonCommand {
-  public async execute(interaction: ButtonInteraction<CacheType>) {
-    const pending = await dataSource.getRepository(Invite).find({
-      where: [
-        {
-          canceled: false,
-          interviewed: false,
-        },
-        {
-          canceled: false,
-          invited: false,
-        },
-      ],
-    });
-    if (!pending.length) {
-      throw new Error(
-        "Cannot create a friend config because there's nobody to contact."
-      );
-    }
-
-    const sorted = pending.sort(sortInvites);
+class FriendConfigButtonCommand extends PendingButtonCommand {
+  protected getContent(pending: Invite[]): string {
     const config = `[Friends]
-${sorted
+${pending
   .slice(0, 100)
   .map((p, i) => p.getFriendEntry(i + 1))
   .join("\n")}`;
-    await interaction.reply({
-      content: `Friend config:
-${config}`,
-      ephemeral: true,
-    });
+    return `Paste into your \`/EverQuest/<CHAR>_P1999Green.ini\` file:
+${config}`;
   }
 }
 
--- src/features/invite-list/invite-command.ts
+++ src/features/invite-list/invite-command.ts
@@ -1,21 +1,6 @@
 import { CacheType, CommandInteraction } from "discord.js";
 import { Command, getOption } from "../../shared/command/command";
-import { dataSource } from "../../db/data-source";
-import { Class, classes } from "../../shared/classes";
-import { Invite } from "../../db/invite";
-import { updateInviteListInfo } from "./update-action";
-
-enum Option {
-  Name = "name",
-  Class = "class",
-  Level = "level",
-  Main = "main",
-}
-
-const classChoices: [name: string, value: string][] = classes.map((c) => [
-  c,
-  c,
-]);
+import { newInvite, classChoices, Option } from "./util";
 
 class InviteCommand extends Command {
   public constructor(
@@ -26,42 +11,12 @@ class InviteCommand extends Command {
     super(name, description);
   }
   public async execute(interaction: CommandInteraction<CacheType>) {
-    const name = String(
-      getOption(Option.Name, interaction)?.value
-    ).toLowerCase();
-    const className = getOption(Option.Class, interaction)?.value;
-    const level = getOption(Option.Level, interaction)?.value;
     const main = getOption(Option.Main, interaction)?.value;
-
-    // check that the name isn't already tracked
-    const invites = await dataSource.getRepository(Invite).findBy({
-      name,
-      invited: false,
-      canceled: false,
+    const invite = await newInvite(interaction, {
+      main: main ? String(main) : undefined,
+      interviewed: true,
     });
-    if (invites.length) {
-      throw new Error(`${name} is already being tracked.`);
-    }
-
-    const invite = new Invite();
-    invite.name = name;
-    invite.byUserId = interaction.user.id;
-    if (level) {
-      invite.level = Number(level);
-    }
-    if (className) {
-      invite.class = className as Class;
-    }
-    if (main) {
-      invite.main = String(main);
-    }
-    invite.interviewed = true;
-
-    await dataSource.manager.save(invite);
-
     interaction.editReply(`Needs invite: ${invite.richLabel}`);
-
-    await updateInviteListInfo(interaction.client);
   }
 
   public get builder() {
