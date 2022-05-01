ce2d439190d3a9b190b3348204fbfb0373e587b1
Scott Goodrow <sgoodrow@gmail.com>
2022-05-01 12:24:18 -0700

    refactor bank hour filenames

--- src/features/bank-hours/command.ts
+++ src/features/bank-hours/command.ts
@@ -1,6 +1,6 @@
 import { Command } from "../../shared/command/command";
-import { addSubcommand } from "./add-command";
-import { removeSubcommand } from "./remove-command";
+import { addSubcommand } from "./add-subcommand";
+import { removeSubcommand } from "./remove-subcommand";
 
 export const bankHourCommand = new Command(
   "bankhour",
