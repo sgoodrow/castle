87314d0dc27917f2feb39109092cef9874ca15d6
Scott Goodrow <sgoodrow@gmail.com>
2022-05-23 23:12:52 -0700

    fix: make it tinier

--- src/features/bank-request-info/update-action.ts
+++ src/features/bank-request-info/update-action.ts
@@ -89,11 +89,10 @@ ${bankHour
   .map((a) => `• ${a.richLabel}`)
   .sort((a, b) => (a > b ? 1 : -1))
   .join("\n")}
+
 ⚠️ **TL;DR** Make requests when you're available and follow the instructions. Bankers will only process requests made in ${
         this.channel
-      } (not PMs).
-
-_last updated <t:${moment().unix()}:R>_`,
+      } (not PMs).`,
       color: "PURPLE",
     });
   }
