1216a8e2cb73848a5411aa7c4ffbc0f1c8c0d63e
Scott Goodrow <sgoodrow@gmail.com>
2023-03-28 22:39:07 -0700

    feat: allow adj 0 on raid bonuses

--- src/features/dkp-records/request-bonus/add-adjustment-bonus.ts
+++ src/features/dkp-records/request-bonus/add-adjustment-bonus.ts
@@ -14,7 +14,7 @@ export class AddAdjustmentBonus extends RaidBonusRequest {
       throw this.getFormatError("missing player name");
     }
     const value = Number(valueString);
-    if (value === 0 || Number.isNaN(value)) {
+    if (Number.isNaN(value)) {
       throw this.getFormatError("invalid adjustment value");
     }
     const reason = reasonWords.join(" ");
