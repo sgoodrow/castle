580f04c83afcdde6be76b03c2791aed96a46e719
Scott Goodrow <sgoodrow@gmail.com>
2023-03-28 18:18:14 -0700

    fix: capture canceled events too?

--- src/listeners/guild-scheduled-event-listener.ts
+++ src/listeners/guild-scheduled-event-listener.ts
@@ -14,8 +14,12 @@ export const guildScheduledEventStartedListener = async (
   before: GuildScheduledEvent,
   after: GuildScheduledEvent
 ) => {
-  if (before.status === after.status || after.status !== "ACTIVE") {
+  // run on status changes
+  if (before.status === after.status) {
     return;
   }
-  recordRaidStarted(client, after);
+  // run when its completed or canceled
+  if (["COMPLETED", "CANCELED"].includes(after.status)) {
+    recordRaidStarted(client, after);
+  }
 };
