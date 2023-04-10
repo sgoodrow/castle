1059f3eefb7df7e990c04f6d281ce64867ca29ed
Schuyler Lindberg <schuyberg@gmail.com>
2023-04-10 08:16:07 +0000

    wip -- reads inventory data

--- src/listeners/message-create-listener.ts
+++ src/listeners/message-create-listener.ts
@@ -2,11 +2,13 @@ import { Message } from "discord.js";
 import { tryCreateRaidReportThreadAction } from "../features/dkp-records/create/create-raid-report-thread";
 import { tryRaidBonusMessageAction } from "../features/dkp-records/request-bonus/request-bonus-message-action";
 import { tryRaidReportRevisionMessageAction } from "../features/dkp-records/request-revision/raid-report-revision-message-action";
+import { tryParseInventoryAction } from "../features/bank-inventory/upload-inventory-file";
 
 const messageActions = [
   tryCreateRaidReportThreadAction,
   tryRaidReportRevisionMessageAction,
   tryRaidBonusMessageAction,
+  tryParseInventoryAction,
 ];
 
 export const messageCreateListener = async (reaction: Message) => {
