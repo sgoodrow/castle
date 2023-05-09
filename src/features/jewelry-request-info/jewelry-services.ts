9f670df656d671e279fa1a8eb721c382b08a2da3
Rocky4686 <130195030+Rocky4686@users.noreply.github.com>
2023-05-09 08:48:28 -0400

    Update jewelry-services.ts
    link update

--- src/features/jewelry-request-info/jewelry-services.ts
+++ src/features/jewelry-request-info/jewelry-services.ts
@@ -3,7 +3,7 @@ import { Icon, Service } from "../bank-request-info/types";
 export const services: Service[] = [
   {
     title: "Jewelry Spreadsheet",
-    inventoryUrl: "https://tinyurl.com/castle-jewelry",
+    inventoryUrl: "https://docs.google.com/spreadsheets/d/1_Wqh3A_0Wg0JN4Fbt06j51yUqUZVKcVDLbSzzJ9FI4k/edit?usp=sharing",
     icon: Icon.Jewelry,
     requestFormats: [
       "Item name, Item count (List of materials provided, Total cost)",
