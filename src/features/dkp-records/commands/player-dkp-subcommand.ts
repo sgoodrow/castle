fdf8ffcf0c4384ebdee0bbb325e829e889ac1579
Scott Goodrow <sgoodrow@gmail.com>
2023-06-15 23:01:32 -0700

    feat: optional spam with player dkp command

--- src/features/dkp-records/commands/player-dkp-subcommand.ts
+++ src/features/dkp-records/commands/player-dkp-subcommand.ts
@@ -1,5 +1,4 @@
 import {
-  AttachmentBuilder,
   CacheType,
   CommandInteraction,
   TimestampStyles,
@@ -11,6 +10,7 @@ import { code } from "../../../shared/util";
 
 enum Option {
   Character = "character",
+  Spam = "spam",
 }
 
 export class PlayerDkpSubcommand extends Subcommand {
@@ -23,6 +23,7 @@ export class PlayerDkpSubcommand extends Subcommand {
       Option.Character,
       interaction
     );
+    const spam = this.getOptionValue<boolean>(Option.Spam, interaction);
     const character = await castledkp.getCharacter(characterName);
     if (!character) {
       throw new Error(`Character ${characterName} does not exist.`);
@@ -33,29 +34,43 @@ export class PlayerDkpSubcommand extends Subcommand {
       net === 0
         ? "0"
         : net > 0
-        ? `+                      ${net}`
-        : `-                      ${net}`;
-    await interaction.editReply({
-      content: `**${points.characterName}** DKP as of ${time(
-        Date.now(),
-        TimestampStyles.ShortDateTime
-      )}${code}diff
+        ? `+                     ${net}`
+        : `-                     ${net}`;
+    const content = `**${points.characterName}** DKP as of ${time(
+      Date.now(),
+      TimestampStyles.ShortDateTime
+    )}${code}diff
 + DKP Earned          ${points.lifetimeDkp}
 - DKP Spent           ${points.spentDkp}
 ----------------------------
-${result}${code}`,
-    });
+${result}${code}`;
+
+    if (spam) {
+      await interaction.channel?.send({ content });
+    } else {
+      await interaction.editReply({
+        content,
+      });
+    }
   }
 
   public get command() {
-    return super.command.addStringOption((o) =>
-      o
-        .setName(Option.Character)
-        .setDescription(
-          "The name of the character whose points you want to review."
-        )
-        .setRequired(true)
-    );
+    return super.command
+      .addStringOption((o) =>
+        o
+          .setName(Option.Character)
+          .setDescription(
+            "The name of the character whose points you want to review."
+          )
+          .setRequired(true)
+      )
+      .addBooleanOption((o) =>
+        o
+          .setName(Option.Spam)
+          .setDescription(
+            "Set to true if you want the response to be sent to the entire channel."
+          )
+      );
   }
 
   public async getOptionAutocomplete() {
