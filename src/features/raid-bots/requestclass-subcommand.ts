d803f3870278a8154d521bfaf971152ceafbc4da
Ed Morgan <edwardmorgan706@gmail.com>
2023-07-28 17:04:28 -0400

    Restoring checkout lock feature

--- src/features/raid-bots/requestclass-subcommand.ts
+++ src/features/raid-bots/requestclass-subcommand.ts
@@ -11,6 +11,7 @@ import { PublicAccountService } from "../../services/public-accounts";
 import moment from "moment";
 import { Class } from "../../shared/classes";
 import { capitalize } from "../../shared/util";
+import { Mutex } from "async-mutex";
 
 export enum Option {
   Class = "class",
@@ -18,8 +19,10 @@ export enum Option {
 }
 
 export class RequestClassSubcommand extends Subcommand {
+  private mutex: Mutex;
   public constructor(name: string, description: string) {
     super(name, description);
+    this.mutex = new Mutex();
   }
 
   public async execute(interaction: CommandInteraction<CacheType>) {
@@ -35,77 +38,81 @@ export class RequestClassSubcommand extends Subcommand {
 
     let status = "✅ ";
     let firstBot = "";
-
+    const release = await this.mutex.acquire();
     try {
-      const publicAccounts = PublicAccountService.getInstance();
-      firstBot = await publicAccounts.getFirstAvailableBotByClass(
-        botClass,
-        location
-      );
-    } catch (err) {
-      status = "❌";
-      await interaction.editReply(
-        `No bot was found matching the provided criteria.`
-      );
-
-      let response = `${interaction.user} requested access to the first available ${botClass}`;
-      if (location) {
-        response += ` in ${location}`;
-      }
-      response += ` and got nothing ${status} (${err})`;
-      await thread.send(response);
-      return;
-    }
-
-    try {
-      const details = await accounts.getAccount(
-        firstBot,
-        interaction.member?.roles as GuildMemberRoleManager
-      );
-
-      await interaction.user
-        .send(`${firstBot} is the first available ${botClass} in the sheet. Your name has automatically been added to the public bot sheet along with a timestamp.\n
-
-${details.characters} (${details.purpose})\n
-Account: ${details.accountName}\n
-Password: ${spoiler(details.password)}\n\n
-
-When you are finished with the bot, please use /bot park <name> <location if you moved it>. This will automatically remove your details from the public sheet`);
-
-      await interaction.editReply(
-        `The credentials for ${firstBot} have been DM'd to you. Please remember to /bot park when you are done with the bot.`
-      );
-
-      // Update public record
       try {
-        const guildUser = await interaction.guild?.members.fetch(
-          interaction.user.id
-        );
-        await PublicAccountService.getInstance().updateBotPilot(
-          firstBot,
-          guildUser?.user.username || "UNKNOWN USER"
-        );
-        await PublicAccountService.getInstance().updateBotCheckoutTime(
-          firstBot,
-          moment()
+        const publicAccounts = PublicAccountService.getInstance();
+        firstBot = await publicAccounts.getFirstAvailableBotByClass(
+          botClass,
+          location
         );
       } catch (err) {
-        throw new Error(
-          "Failed to update public record, check the configuration"
+        status = "❌";
+        await interaction.editReply(
+          `No bot was found matching the provided criteria.`
+        );
+
+        let response = `${interaction.user} requested access to the first available ${botClass}`;
+        if (location) {
+          response += ` in ${location}`;
+        }
+        response += ` and got nothing ${status} (${err})`;
+        await thread.send(response);
+        return;
+      }
+
+      try {
+        const details = await accounts.getAccount(
+          firstBot,
+          interaction.member?.roles as GuildMemberRoleManager
+        );
+
+        await interaction.user
+          .send(`${firstBot} is the first available ${botClass} in the sheet. Your name has automatically been added to the public bot sheet along with a timestamp.\n
+  
+  ${details.characters} (${details.purpose})\n
+  Account: ${details.accountName}\n
+  Password: ${spoiler(details.password)}\n\n
+  
+  When you are finished with the bot, please use /bot park <name> <location if you moved it>. This will automatically remove your details from the public sheet`);
+
+        await interaction.editReply(
+          `The credentials for ${firstBot} have been DM'd to you. Please remember to /bot park when you are done with the bot.`
+        );
+
+        // Update public record
+        try {
+          const guildUser = await interaction.guild?.members.fetch(
+            interaction.user.id
+          );
+          await PublicAccountService.getInstance().updateBotPilot(
+            firstBot,
+            guildUser?.user.username || "UNKNOWN USER"
+          );
+          await PublicAccountService.getInstance().updateBotCheckoutTime(
+            firstBot,
+            moment()
+          );
+        } catch (err) {
+          throw new Error(
+            "Failed to update public record, check the configuration"
+          );
+        }
+      } catch (err) {
+        status = "❌";
+
+        await interaction.editReply(
+          `You do not have the correct permissions to access ${firstBot}.`
         );
       }
-    } catch (err) {
-      status = "❌";
 
-      await interaction.editReply(
-        `You do not have the correct permissions to access ${firstBot}.`
+      const logMsg = await thread.send("OK");
+      logMsg.edit(
+        `${interaction.user} requested access to the first available ${botClass} and got ${firstBot} ${status}`
       );
+    } finally {
+      release();
     }
-
-    const logMsg = await thread.send("OK");
-    logMsg.edit(
-      `${interaction.user} requested access to the first available ${botClass} and got ${firstBot} ${status}`
-    );
   }
 
   public get command() {
