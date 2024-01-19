00caa259820dc9383ed3d2d61a0505fb7dcfcc08
Boosted20 <147990549+Boosted20@users.noreply.github.com>
2024-01-18 19:39:20 -0600

    Update config.ts
    Added additional instructions to some applications (making a list of alts and DMing officers with the application)

--- src/features/applications/config.ts
+++ src/features/applications/config.ts
@@ -9,6 +9,7 @@ const officerApplicationCommand = new RequestApplication(
     "Officers must set a shining example for the other members of the guild. This may include moderating our typical habits of speech in guild chat, and refraining from trash-talking even when others are rude. Furthermore, we must know the server rules inside and out, and follow them. Our behavior reflects directly upon Castle itself. Are you willing to hold yourself to the highest standards of decorum even during emotional times and to take responsibility for the consequences of your decisions as an officer?",
     "While we are allowed to play the game and not be on-duty all the time, being an Officer will indeed consume some of one's game time. Are you willing to dedicate some of your game time to being an Officer of Castle? And do you have the wisdom to set aside time for yourself to enjoy the game?",
     "The complaints and reports we receive are sometimes genuine, but sometimes frivolous or false. We need people who will endure the discomfort of dealing diplomatically with reports and be willing to have honest talks with members, mostly 30-to-60-year-old adults, about their misbehavior in a video game. Do you have the maturity to maintain your composure and behave professionally?",
+    "Please include a list with the name, class and level of all of your characters that are tagged in the alliance. Once you have filled out this application and created the list please DM this to an officer.",
   ],
   "current officers. Each applicant must be approved by 3 current officers and not be opposed by any"
 );
@@ -21,6 +22,7 @@ const guardApplicationCommand = new RequestApplication(
     "Please describe what motivates you to volunteer as a Guard.",
     "Guards must set a shining example of the guild while interacting with prospective recruits. Are you willing to hold yourself to the highest standards of decorum while representing Castle to new members?",
     "While we should play the game and not be on-duty all the time, being a Guard will indeed consume some of one's game time. Are you willing to dedicate some of your game time to being a Guard of Castle? And do you have the wisdom to set aside time for yourself to enjoy the game?",
+    "Please include a list with the name, class and level of all of your characters that are tagged in the alliance. Once you have filled out this application and created the list please DM this to an officer.",
   ],
   "current officers, and approved by the Castle Captain of the Guard"
 );
@@ -33,6 +35,7 @@ const knightApplicationCommand = new RequestApplication(
     "Please describe what motivates you to volunteer as a Knight. Which aspects of being raid leadership, support and preparation are you interested in or willing to help with?",
     "Knights must set a shining example of the guild while interacting with raiders. Are you willing to hold yourself to the highest standards of decorum while representing Castle to new members?",
     "While we should play the game and not be on-duty all the time, being a Knight will indeed consume some of one's game time. Are you willing to dedicate some of your game time to being a Knight of Castle? And do you have the wisdom to set aside time for yourself to enjoy the game?",
+    "Once you have filled out this application, please DM this to an officer.",
   ],
   "the current Knight Commander and a conversation ensues regarding what you want to support. Then, current officers vote, and the application is approved by the Castle Knight Commander"
 );
@@ -45,6 +48,7 @@ const bankerApplicationCommand = new RequestApplication(
     "Banking requires you to perform recordkeeping in various places including google docs and discord threads. Are you willing to dedicate some of your game time to running the guild bank? And do you have the wisdom to set aside time for yourself to enjoy the game?",
     "Banking requires patience and attention to detail. Are you capable of being patient and performing all aspects of banking tasks?",
     "How would you respond to a request by an individual who is not in zone when you log in to fullfill their request?",
+    "Once you have filled out this application, please DM this to an officer.",
   ],
   "current officers, and approved by the Castle Treasurer"
 );
@@ -57,6 +61,7 @@ const jewelerApplicationCommand = new RequestApplication(
     "Jewlery crafting requires you to perform recordkeeping in various places including google docs and discord threads. Are you willing to dedicate some of your game time to being a guild jewelers? And do you have the wisdom to set aside time for yourself to enjoy the game?",
     "Jewlery crafting requires patience and attention to detail. Are you capable of being patient and performing all aspects of jewelry crafting tasks?",
     "How would you respond to a request by an individual who is not in zone when you log in to fullfill their request?",
+    "Once you have filled out this application, please DM this to an officer.",
   ],
   "current officers, and approved by the Castle Treasurer"
 );
