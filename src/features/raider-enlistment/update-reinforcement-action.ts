5c1a7ad70762ff766878125f3173996aa0d213b5
Scott <sgoodrow@gmail.com>
2024-04-14 22:36:01 -0700

    Update update-reinforcement-action.ts

--- src/features/raider-enlistment/update-reinforcement-action.ts
+++ src/features/raider-enlistment/update-reinforcement-action.ts
@@ -36,14 +36,14 @@ class UpdateReinforcementsInfoAction extends InstructionsReadyAction {
   private async getDescriptionEmbed() {
     const role = `<@&${reinforcementsRoleId}>`;
     return new EmbedBuilder({
-      title: "Join the Reinforcements",
+      title: "# Join the Reinforcements",
       description: `The ${role} role is **optional**. Are you available to assist with ad hoc targets of opportunity that typically require 1-2 groups such as Scout, VSR, Ragefire, etc? Join the reinforcements!
 
-❓ **How do I participate and how do I stop?**
+### ❓ **How do I participate and how do I stop?**
 • Use the buttons below to join and leave the reinforcements at will.
 • When called upon: if you don't like the loot rules, don't help. Play nice with each other and be fair.
 
-❓ **How do I call for reinforcements?**
+### ❓ **How do I call for reinforcements?**
 • Ping the role when you need help.
 • You are responsible for determining fair loot rules and ensuring they are understood by all participants.
 • Avoid asking for help during raids and scheduled socks.
@@ -51,7 +51,7 @@ class UpdateReinforcementsInfoAction extends InstructionsReadyAction {
 • Avoid using for filling out EXP groups.
 • These events cannot involve earning or spending DKP.
 
-❗ **Examples**
+### ❗ **Examples**
 > "${role} I won Scout roll and need help. Quest loot is mine, rest /random"
 
 > "${role} Phinny is up, all loot /random"
