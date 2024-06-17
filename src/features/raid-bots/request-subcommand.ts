8891b2a09fdc55312dfc4ad2e9d5f766d66d7ece
ebm5025 <ebm5025@gmail.com>
2024-06-17 16:16:34 -0400

    Don't update public sheet with pilot if checkout failed (#114)
    * Don't update public sheet with pilot if checkout failed
    
    * Ensure failures are logged in request log

--- src/features/raid-bots/request-subcommand.ts
+++ src/features/raid-bots/request-subcommand.ts
@@ -50,37 +50,39 @@ Password: ${spoiler(details.password)}
         response += `** Please note that ${currentPilot} is marked as the pilot of ${name} and you may not be able to log in. Your name will not be added as the botpilot in the public bot sheet! **\n\n`;
       }
       response += `The credentials for ${name} have been DM'd to you. Please remember to use \`/bot park\` when you are done!`;
+
+      const logMsg = await thread.send("OK");
+      logMsg.edit(`${status} ${interaction.user} access to ${name}.`);
+
+      if (await publicAccounts.isBotPublic(name)) {
+        try {
+          const guildUser = await interaction.guild?.members.fetch(
+            interaction.user.id
+          );
+
+          if (!currentPilot) {
+            await publicAccounts.updateBotRowDetails(name, {
+              [BOT_SPREADSHEET_COLUMNS.CurrentPilot]:
+                guildUser?.user.username || "UNKNOWN USER",
+              [BOT_SPREADSHEET_COLUMNS.CheckoutTime]: moment().toString(),
+            });
+          }
+        } catch (err) {
+          throw new Error(
+            "Failed to update public record, check the configuration"
+          );
+        }
+      }
+
       await interaction.editReply(response);
     } catch (err) {
       status = "❌ Denied";
+      const logMsg = await thread.send("OK");
+      logMsg.edit(`${status} ${interaction.user} access to ${name}.`);
 
       await interaction.editReply(
         `You do not have the correct permissions to access ${name}.`
       );
-    }
-
-    const logMsg = await thread.send("OK");
-    logMsg.edit(`${status} ${interaction.user} access to ${name}.`);
-
-    // Update public record
-    if (await publicAccounts.isBotPublic(name)) {
-      try {
-        const guildUser = await interaction.guild?.members.fetch(
-          interaction.user.id
-        );
-
-        if (!currentPilot) {
-          await publicAccounts.updateBotRowDetails(name, {
-            [BOT_SPREADSHEET_COLUMNS.CurrentPilot]:
-              guildUser?.user.username || "UNKNOWN USER",
-            [BOT_SPREADSHEET_COLUMNS.CheckoutTime]: moment().toString(),
-          });
-        }
-      } catch (err) {
-        throw new Error(
-          "Failed to update public record, check the configuration"
-        );
-      }
     }
   }
 
