1d3ab162a82a7d84efa0b71fd8fcc588d654e2e1
Ed Morgan <edwardmorgan706@gmail.com>
2024-06-22 13:22:57 -0400

    Added test command

---
+++ src/features/wakeup/wakeuptest-command.ts
@@ -0,0 +1,56 @@
+import {
+  ApplicationCommandOptionChoiceData,
+  AutocompleteInteraction,
+  CacheType,
+  CommandInteraction,
+  ThreadMemberFlagsBitField,
+} from "discord.js";
+import { Subcommand } from "../../shared/command/subcommand";
+import { authorizeByMemberRoles } from "../../shared/command/util";
+import {
+  officerRoleId,
+  knightRoleId,
+  wakeupChannelId,
+  modRoleId,
+} from "../../config";
+import { container } from "tsyringe";
+import { WakeupService } from "./wakeup.service";
+
+export class WakeupTestSubcommand extends Subcommand {
+  
+  constructor(name: string, description: string) {
+    super(name, description);
+  }
+
+  public async execute(interaction: CommandInteraction<CacheType>) {
+    authorizeByMemberRoles(
+      [officerRoleId, modRoleId, knightRoleId],
+      interaction
+    );
+    if (wakeupChannelId) {
+      const wakeupService = container.resolve(WakeupService);
+      await wakeupService.runWakeup(
+        `Batphone. ${interaction.user} sent a test command`
+      );
+      await interaction.editReply("Test command executed");
+    }
+  }
+
+  public get command() {
+    return super.command;
+  }
+
+  public getOptionAutocomplete(
+    option: string,
+    interaction: AutocompleteInteraction<CacheType>
+  ): Promise<
+    ApplicationCommandOptionChoiceData<string | number>[] | undefined
+  > {
+    throw new Error("Method not implemented.");
+  }
+}
+
+export const wakeupTestSubCommand = new WakeupTestSubcommand(
+  "test",
+  "Test the wakeup"
+);
