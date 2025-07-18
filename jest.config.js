412c9cdda835dc51526684cb1c367b4324855150
Scott Goodrow <sgoodrow@gmail.com>
2025-07-18 08:57:48 -0700

    fix: ci?

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-undef */
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  setupFilesAfterEnv: ["<rootDir>/src/test/setup.ts"],
 };
