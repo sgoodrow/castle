dd82f932e12636da34cdce94c35e3de4cc8e15d4
Ed Morgan <edwardmorgan706@gmail.com>
2026-01-04 23:37:02 -0500

    Fix timestamp format for adjustments

--- src/features/dkp-records/request-bonus/add-adjustment-bonus.ts
+++ src/features/dkp-records/request-bonus/add-adjustment-bonus.ts
@@ -3,19 +3,17 @@ import { castledkp } from "../../../services/castledkp";
 import { RaidBonusRequest } from "./raid-bonus-request";
 import { openDkpService } from "../../../services/openDkpService";
 import moment from "moment";
-import { UPLOAD_DATE_FORMAT } from "../raid-tick";
 
 export class AddAdjustmentBonus extends RaidBonusRequest {
   protected async execute(raidId: number) {
     const adjustment = await this.validateArgs();
-    const now = moment().format(UPLOAD_DATE_FORMAT);
     await castledkp.addAdjustment(raidId, adjustment);
     await openDkpService.addAdjustment({
       Character: { Name: adjustment.player },
       Description: adjustment.reason,
       Name: adjustment.reason,
       Value: adjustment.value,
-      Timestamp: now,
+      Timestamp: moment().toISOString(),
     });
   }
 
