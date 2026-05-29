60d28f09c8aa6dd4610ffbdd94826de1e03df5d4
ebm5025 <ebm5025@gmail.com>
2026-05-28 22:04:07 -0400

    Add info command (#252)
    Co-authored-by: Ed Morgan <edwardmorgan706@gmail.com>

--- src/features/dkp-records/commands/raid-command.ts
+++ src/features/dkp-records/commands/raid-command.ts
@@ -1,11 +1,13 @@
 import { Command } from "../../../shared/command/command";
 import { bonusesThreadSubcommand } from "./bonuses-thread-subcommand";
 import { getValuesSubcommand } from "./get-values-subcommand";
+import { infoSubcommand } from "./info-subcommand";
 import { setTickSubcommand } from "./set-tick-subcommand";
 
 export const raidCommand = new Command("raid", "Work with raids.", [
   setTickSubcommand,
   bonusesThreadSubcommand,
-  getValuesSubcommand
+  getValuesSubcommand,
+  infoSubcommand
   //getPlayerDkpSubcommand,
 ]);
